580ec68bd8b9ed6391d1a36b06076eb408cad951
Diana <dianaaranda14@gmail.com>
2023-06-01 18:07:21 -0700

    Created dictionary and sorted by keys

--- ratings.py
+++ ratings.py
@@ -1,4 +1,19 @@
 """Restaurant rating lister."""
 
+file = open("scores.txt")
 
-# put your code here
+scores = {}
+
+for line in file:
+    line = line.rstrip()
+    restaurant, rating = line.split(":")
+    scores[restaurant] = rating
+    
+    # sorted_scores = sorted(scores.items())
+
+
+for key, value in scores.items():
+    print(f"{key} is rated at {value}")
+
+
+
